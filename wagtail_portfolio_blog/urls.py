9faec6e8f598a18f3fa99f59e0cd951260f949be
korea7030 <korea7030@naver.com>
2022-01-03 22:11:12 +0900

    wagtail sitemap

--- wagtail_portfolio_blog/urls.py
+++ wagtail_portfolio_blog/urls.py
@@ -5,6 +5,7 @@ from django.urls import path
 
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.core import urls as wagtail_urls
+from wagtail.contrib.sitemaps.views import sitemap
 from wagtail.documents import urls as wagtaildocs_urls
 
 from search import views as search_views
@@ -16,6 +17,8 @@ urlpatterns = [
     path('documents/', include(wagtaildocs_urls)),
 
     path('search/', search_views.search, name='search'),
+    path('sitemap.xml', sitemap),
+
 
 ]
 
